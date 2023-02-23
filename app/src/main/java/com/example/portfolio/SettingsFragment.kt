5e87e552f9e71f8b5d442e4d4ac067488f4ae752
Luís Fernando <lfbsdev@gmail.com>
2023-02-23 17:11:51 -0300

    feat: add light/dark theme switch preference
    delete old Preference at root_preferences.xml
    add SwitchPreference, set its onChangeListener
    add function to handle theme changes

--- app/src/main/java/com/example/portfolio/SettingsFragment.kt
+++ app/src/main/java/com/example/portfolio/SettingsFragment.kt
@@ -1,11 +1,22 @@
 package com.example.portfolio
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.SwitchPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+        val themePreference = preferenceManager.findPreference<SwitchPreference>("theme_preference")
+        themePreference?.setOnPreferenceChangeListener{ _, value ->
+            setThemeMode(value as Boolean)
+            true
+        }
     }
+}
+
+private fun setThemeMode(isLight: Boolean){
+    if (isLight) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 }
