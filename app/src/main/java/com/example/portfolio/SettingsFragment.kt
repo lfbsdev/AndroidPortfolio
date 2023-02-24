314035d5f7b1937c60d9c56a13ce6194b1e32fd2
Luís Fernando <lfbsdev@gmail.com>
2023-02-24 16:14:21 -0300

    fix: change theme takes two taps
    if the theme switch is left on when leaving the app, it will remain on when coming back, requiring two taps to switch back.
    
    - setThemeMode() replaced by toggleThemeMode()
    - toggleThemeMode() simply flips the current theme

--- app/src/main/java/com/example/portfolio/SettingsFragment.kt
+++ app/src/main/java/com/example/portfolio/SettingsFragment.kt
@@ -1,22 +1,25 @@
 package com.example.portfolio
 
+import android.content.res.Configuration.*
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
 import androidx.preference.SwitchPreference
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
         val themePreference = preferenceManager.findPreference<SwitchPreference>("theme_preference")
-        themePreference?.setOnPreferenceChangeListener{ _, value ->
-            setThemeMode(value as Boolean)
+        themePreference?.setOnPreferenceChangeListener{ _, _ ->
+            toggleThemeMode(preferenceScreen)
             true
         }
     }
 }
 
-private fun setThemeMode(isLight: Boolean){
-    if (isLight) AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-    else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+private fun toggleThemeMode(preferenceScreen: PreferenceScreen){
+    val nightModeFlags: Int = preferenceScreen.context.resources.configuration.uiMode and UI_MODE_NIGHT_MASK
+    val oppositeNightMode: Int = if (nightModeFlags == UI_MODE_NIGHT_NO) AppCompatDelegate.MODE_NIGHT_YES else AppCompatDelegate.MODE_NIGHT_NO
+    AppCompatDelegate.setDefaultNightMode(oppositeNightMode)
 }
