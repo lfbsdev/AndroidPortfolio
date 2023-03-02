a08625c1ec3f4c2c57e012a6ab4bc3ab1c6cb176
Luís Fernando <lfbsdev@gmail.com>
2023-03-02 15:38:02 -0300

    refactor: username is now an email
    - isUserNameValid only returns true if it is an email
    - rename and edit invalid username string resource to invalid email
    - replace userId with email, delete displayName

--- app/src/main/java/com/example/portfolio/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/portfolio/ui/login/LoginViewModel.kt
@@ -31,7 +31,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
 
     fun loginDataChanged(username: String, password: String) {
         if (!isUserNameValid(username)) {
-            _loginForm.value = LoginFormState(usernameError = R.string.invalid_username)
+            _loginForm.value = LoginFormState(usernameError = R.string.invalid_email)
         } else if (!isPasswordValid(password)) {
             _loginForm.value = LoginFormState(passwordError = R.string.invalid_password)
         } else {
@@ -43,9 +43,7 @@ class LoginViewModel(private val loginRepository: LoginRepository) : ViewModel()
     private fun isUserNameValid(username: String): Boolean {
         return if (username.contains('@')) {
             Patterns.EMAIL_ADDRESS.matcher(username).matches()
-        } else {
-            username.isNotBlank()
-        }
+        } else false
     }
 
     // A placeholder password validation check
